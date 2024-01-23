4d6d3b63cfe6d20eca22db8a63734b41d2c2d012
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-24 00:06:05 +0100

    implemented register and login page, installed toastify

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,7 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
+
+import RegisterWrapper from "../assets/wrappers/RegisterPage";
+import { FormRow, Logo } from "../components";
+import { toast } from "react-toastify";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
 
 function Register() {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    console.log(`${name}:${value}`);
+    setValues({ ...values, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      toast.error("Please Fill Out All Fields");
+      return;
+    }
+  };
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+  return (
+    <RegisterWrapper className="full-page">
+      <form className="form" onSubmit={handleSubmit}>
+        <Logo />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {!values.isMember && (
+          <FormRow
+            name="name"
+            type="text"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
+        <FormRow
+          name="email"
+          type="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        <FormRow
+          name="password"
+          type="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <div className="form-row">
+          <button type="submit" className="btn btn-block">
+            Submit
+          </button>
+          <button type="button" className="btn btn-block btn-hipster">
+            Demo App
+          </button>
+          <p>
+            {values.isMember ? "Not a member yet?" : "Already a member?"}
+            <button type="button" onClick={toggleMember} className="member-btn">
+              {values.isMember ? "Register" : "Login"}
+            </button>
+          </p>
+        </div>
+      </form>
+    </RegisterWrapper>
+  );
 }
 
 export default Register;
