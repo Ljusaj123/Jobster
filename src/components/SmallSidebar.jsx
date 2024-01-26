36537e40a258189a1645e59268b688c719f845a3
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-27 00:23:05 +0100

    created links, navlinks component and structure for small sidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,7 +1,35 @@
-import SmallSidebarWrapper from "../assets/wrappers/SmallSidebar";
+import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleSidebar } from "../utils/userSlice";
+import NavLinks from "./NavLinks";
 
-function SmallSidebar() {
-  return <SmallSidebarWrapper>SmallSidebar</SmallSidebarWrapper>;
-}
+export const SmallSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+  const toggle = () => {
+    dispatch(toggleSidebar());
+  };
+  return (
+    <Wrapper>
+      <div
+        className={
+          isSidebarOpen ? "sidebar-container show-sidebar" : "sidebar-container"
+        }
+      >
+        <div className="content">
+          <button className="close-btn" onClick={toggle}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <NavLinks toggleSidebar={toggle} />
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
 
 export default SmallSidebar;
