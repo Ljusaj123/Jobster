fa4efb1c07d8909bc483d858ed76dee40539d555
Ljusaj123 <rosana.ljusaj2@gmail.com>
2024-01-27 00:41:02 +0100

    big sidebar structure

--- src/components/BigSidebar.jsx
+++ src/components/BigSidebar.jsx
@@ -1,7 +1,28 @@
-import BigSidebarWrapper from "../assets/wrappers/BigSidebar.js";
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
+import BigSidebarWrapper from "../assets/wrappers/BigSidebar";
+import { useSelector } from "react-redux";
 
-function BigSidebar() {
-  return <BigSidebarWrapper>BigSidebar</BigSidebarWrapper>;
-}
+const BigSidebar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
+  return (
+    <BigSidebarWrapper>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
+    </BigSidebarWrapper>
+  );
+};
 
 export default BigSidebar;
